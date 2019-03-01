ede7b3344a43882421ff1bdb2ae404dde300fd6b
Minhyuk Park <minpark815@gmail.com>
2019-03-01 00:54:54 +0000

    Update api cross origin

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jennifer.minhyukpark.com");
-  res.header("Access-Control-Allow-Origin", "https://chess.minhyukpark.com");
+  var allowedOrigins = ['https://jennifer.minhyukpark.com', 'https://chess.minhyukpark.com']
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  return next();
 });
 
 routes(app);
