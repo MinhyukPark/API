6eebd9ed8f8f1a96bdcaa2f0f5da850e5f0c1286
Minhyuk Park <minpark815@gmail.com>
2019-03-11 18:46:36 +0000

    Add support for mongodb

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 // app.js
 var express = require("express");
+var cors = require("cors");
 var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+var allowedOrigins = ['https://jennifer.minhyukpark.com', 'https://chess.minhyukpark.com', 'https://music.minhyukpark.com', 'https://api.minhyukpark.com'];
+var corsOptions = {
+    origin: function(origin, callback) {
+        console.log(origin);
+        if(allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: "GET, POST, OPTIONS",
+    optionsSuccessStatus: 204,
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, roomcode"
+}
+
+app.use(cors(corsOptions));
+//app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+/*
 app.use(function(req, res, next) {
-  var allowedOrigins = ['https://jennifer.minhyukpark.com', 'https://chess.minhyukpark.com']
-  var origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
   return next();
 });
+*/
 
 routes(app);
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,18 @@
 // routes.js
 var fs = require("fs");
+var prometheus_cred = require('../prometheus_cred.json');
+var monitor_cred = require('../monitor_cred.json');
+var office_cred = require('../office_cred.json');
+var MongoClient = require('mongodb').MongoClient;
+const auth = require('express-basic-auth');
+var g_client = null;
+MongoClient.connect('mongodb://localhost:27017/music', (err, client) => {
+    g_client = client;
+});
+
 var appRouter = function (app) {
   app.get("/", function(req, res) {
-    res.status(200).send("Ever wonder where jennifer is?");
+    res.status(200).send("central api for lots of things");
   });
 
   app.get("/isjenniferhere", function (req, res) {
@@ -22,7 +32,7 @@ var appRouter = function (app) {
       });
       res.status(200).send(data);
     });
-  })
+  });
 
   app.get("/chess", function (req, res) {
     fs.readFile(__dirname + "/../" + "chess.json", "utf8", function (err, data) {
@@ -41,7 +51,71 @@ var appRouter = function (app) {
       });
       res.status(200).send(data);
     });
-  })
+  });
+
+  app.get("/music", async function (req, res) {
+      var root_db = g_client.db("music");
+      const rooms = root_db.collection("rooms");
+      var data = await rooms.find({"roomcode" : req.get("roomcode")}).toArray();
+          //data = JSON.parse(data);
+      res.status(200).send(data);
+  });
+
+  app.post('/music', async function (req, res) {
+      var root_db = g_client.db("music");
+      const rooms = root_db.collection("rooms");
+      // var data = await rooms.find({"roomcode" : req["body"]["roomcode"]}).toArray();
+      var new_data = req["body"];
+      delete new_data["_id"];
+      console.log("after deletion" + new_data);
+      //data = JSON.parse(data);
+      rooms.replaceOne({"roomcode": req["body"]["roomcode"]}, new_data);
+      res.status(200).send(new_data);
+  });
+
+  app.post('/music/create', function (req, res) {
+      //MongoClient.connect('mongodb://localhost:27017/music', (err, client) => {
+      var root_db = g_client.db("music");
+      const rooms = root_db.collection("rooms");
+      var data = req["body"];
+      //data = JSON.parse(data);
+      rooms.insertOne(data);
+      res.status(200).send(data);
+          //client.close();
+      //});
+  });
+
+  /* auth */
+  app.get('/auth-prometheus', auth({
+    users: prometheus_cred,
+    challenge: true,
+    realm: 'credentials for prometheus.minhyukpark.com'
+  }));
+
+  app.get('/auth-prometheus', function(req, res) {
+    res.status(200).send("authorized");
+  });
+
+  app.get('/auth-monitor', auth({
+    users: monitor_cred,
+    challenge: true,
+    realm: 'credentials for monitor.minhyukpark.com'
+  }));
+
+  app.get('/auth-monitor', function(req, res) {
+    res.status(200).send("authorized");
+  });
+
+  app.get('/auth-office', auth({
+    users: office_cred,
+    challenge: true,
+    realm: 'credentials for office.minhyukpark.com'
+  }));
+
+  app.get('/auth-office', function(req, res) {
+    res.status(200).send("authorized");
+  });
+  /* auth end */
 }
 
 module.exports = appRouter;
